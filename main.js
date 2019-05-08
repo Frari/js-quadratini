cc7696c7ae602e2b6944d0b58d5f2095b62f95aa
Frari <francescoriccardi86@gmail.com>
2019-05-08 21:02:47 +0200

    fine

--- main.js
+++ main.js
@@ -2,10 +2,27 @@
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
 // Parte 2 (Bonus)
 // Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
+
+
 $('.quadrato').click(function(){
   $(this).addClass('green');
 });
 
 $('.quadrato1').click(function(){
   $(this).addClass('red');
+});
+
+var i=0;
+var j=0;
+
+$('.quadrato').click(function(){
+  i++
+  $('.verdi').text(i);
+  console.log(i);
+});
+
+$('.quadrato1').click(function(){
+  j++
+  $('.rossi').text(j);
+  console.log(j);
 });
